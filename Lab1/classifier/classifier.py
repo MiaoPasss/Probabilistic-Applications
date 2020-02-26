dd58055cd05aa96004f566117bc8f1b33eb1c2da
MiaoPass <hubery.luo@icloud.com>
2020-02-26 17:11:20 -0500

    Finished Lab 1

--- Lab1/classifier/classifier.py
+++ Lab1/classifier/classifier.py
@@ -6,25 +6,48 @@ import util
 def learn_distributions(file_lists_by_category):
     """
     Estimate the parameters p_d, and q_d from the training set
-    
+
     Input
     -----
-    file_lists_by_category: A two-element list. The first element is a list of 
+    file_lists_by_category: A two-element list. The first element is a list of
     spam files, and the second element is a list of ham files.
 
     Output
     ------
-    probabilities_by_category: A two-element tuple. The first element is a dict 
+    probabilities_by_category: A two-element tuple. The first element is a dict
     whose keys are words, and whose values are the smoothed estimates of p_d;
-    the second element is a dict whose keys are words, and whose values are the 
-    smoothed estimates of q_d 
+    the second element is a dict whose keys are words, and whose values are the
+    smoothed estimates of q_d
     """
-    ### TODO: Write your code here
-    
-    
+
+    spam_words = get_word_freq(file_lists_by_category[0])
+    ham_words = get_word_freq(file_lists_by_category[1])
+
+    vocab_set = set(spam_words.keys()).union(set(ham_words.keys()))
+    spam_bag = sum(spam_words.values())
+    ham_bag = sum(ham_words.values())
+
+    pd_dict = {}
+    qd_dict = {}
+
+    for word in vocab_set:
+        if word not in spam_words.keys():
+            spam_words[word] = 0
+        if word not in ham_words.keys():
+            ham_words[word] = 0
+
+    for word, freq in spam_words.items():
+        p_d = (freq + 1) / (spam_bag + len(vocab_set))
+        pd_dict[word] = p_d
+
+    for word, freq in ham_words.items():
+        q_d = (freq + 1) / (ham_bag + len(vocab_set))
+        qd_dict[word] = q_d
+
+    probabilities_by_category = (pd_dict, qd_dict)
     return probabilities_by_category
 
-def classify_new_email(filename,probabilities_by_category,prior_by_category):
+def classify_new_email(filename,probabilities_by_category,prior_by_category, hypothesis_crit = 0):
     """
     Use Naive Bayes classification to classify the email in the given file.
 
@@ -32,24 +55,46 @@ def classify_new_email(filename,probabilities_by_category,prior_by_category):
     ------
     filename: name of the file to be classified
     probabilities_by_category: output of function learn_distributions
-    prior_by_category: A two-element list as [\pi, 1-\pi], where \pi is the 
+    prior_by_category: A two-element list as [\pi, 1-\pi], where \pi is the
     parameter in the prior class distribution
 
     Output
     ------
     classify_result: A two-element tuple. The first element is a string whose value
-    is either 'spam' or 'ham' depending on the classification result, and the 
-    second element is a two-element list as [log p(y=1|x), log p(y=0|x)], 
+    is either 'spam' or 'ham' depending on the classification result, and the
+    second element is a two-element list as [log p(y=1|x), log p(y=0|x)],
     representing the log posterior probabilities
     """
-    ### TODO: Write your code here
-    
-    
+
+    word_bag = get_word_freq([filename])
+    p_d, q_d = probabilities_by_category
+    prior1 = prior_by_category[0]
+    prior0 = 1 - prior1
+
+    # hypothesis_crit = 0 / np.Infinity / np.NINF
+    likelihood1 = 0
+    likelihood0 = 0
+
+    for word, freq in word_bag.items():
+        if word in p_d.keys():
+            likelihood1 += np.log(p_d[word]) * freq
+            likelihood0 += np.log(q_d[word]) * freq
+
+    post_h1 = np.exp(likelihood1 / 10000) / (np.exp(likelihood0 / 10000) + np.exp(likelihood1 / 10000))
+    post_h0 = np.exp(likelihood0 / 10000) / (np.exp(likelihood0 / 10000) + np.exp(likelihood1 / 10000))
+
+    if likelihood1 - likelihood0 > hypothesis_crit:
+        result_string = 'spam'
+    else:
+        result_string = 'ham'
+
+    classify_result = (result_string, [np.log(post_h1), np.log(post_h0)])
+
     return classify_result
 
 if __name__ == '__main__':
-    
-    # folder for training and testing 
+
+    # folder for training and testing
     spam_folder = "data/spam"
     ham_folder = "data/ham"
     test_folder = "data/testing"
@@ -58,13 +103,13 @@ if __name__ == '__main__':
     file_lists = []
     for folder in (spam_folder, ham_folder):
         file_lists.append(util.get_files_in_folder(folder))
-        
-    # Learn the distributions    
+
+    # Learn the distributions
     probabilities_by_category = learn_distributions(file_lists)
-    
+
     # prior class distribution
     priors_by_category = [0.5, 0.5]
-    
+
     # Store the classification results
     performance_measures = np.zeros([2,2])
     # explanation of performance_measures:
@@ -72,10 +117,10 @@ if __name__ == '__main__':
     # rows correspond to true label, columns correspond to guessed label
     # to be more clear, performance_measures = [[p1 p2]
     #                                           [p3 p4]]
-    # p1 = Number of emails whose true label is 'spam' and classified as 'spam' 
-    # p2 = Number of emails whose true label is 'spam' and classified as 'ham' 
-    # p3 = Number of emails whose true label is 'ham' and classified as 'spam' 
-    # p4 = Number of emails whose true label is 'ham' and classified as 'ham' 
+    # p1 = Number of emails whose true label is 'spam' and classified as 'spam'
+    # p2 = Number of emails whose true label is 'spam' and classified as 'ham'
+    # p3 = Number of emails whose true label is 'ham' and classified as 'spam'
+    # p4 = Number of emails whose true label is 'ham' and classified as 'ham'
 
     # Classify emails from testing set and measure the performance
     for filename in (util.get_files_in_folder(test_folder)):
@@ -83,10 +128,10 @@ if __name__ == '__main__':
         label,log_posterior = classify_new_email(filename,
                                                  probabilities_by_category,
                                                  priors_by_category)
-        
+
         # Measure performance (the filename indicates the true label)
         base = os.path.basename(filename)
-        true_index = ('ham' in base) 
+        true_index = ('ham' in base)
         guessed_index = (label == 'ham')
         performance_measures[int(true_index), int(guessed_index)] += 1
 
@@ -96,10 +141,52 @@ if __name__ == '__main__':
     # totals are obtained by summing across guessed labels
     totals = np.sum(performance_measures, 1)
     print(template % (correct[0],totals[0],correct[1],totals[1]))
-    
-    
-    ### TODO: Write your code here to modify the decision rule such that
-    ### Type 1 and Type 2 errors can be traded off, plot the trade-off curve
-   
 
- 
+
+    error1_set = []
+    error2_set = []
+    hypothesis_set = [np.NINF, -140, -100, -60, -20, 20, 60, 100, 140, np.Infinity]
+    for hypothesis_crit in hypothesis_set:
+        # Store the classification results
+        performance_measures = np.zeros([2,2])
+        # explanation of performance_measures:
+        # columns and rows are indexed by 0 = 'spam' and 1 = 'ham'
+        # rows correspond to true label, columns correspond to guessed label
+        # to be more clear, performance_measures = [[p1 p2]
+        #                                           [p3 p4]]
+        # p1 = Number of emails whose true label is 'spam' and classified as 'spam'
+        # p2 = Number of emails whose true label is 'spam' and classified as 'ham'
+        # p3 = Number of emails whose true label is 'ham' and classified as 'spam'
+        # p4 = Number of emails whose true label is 'ham' and classified as 'ham'
+
+        # Classify emails from testing set and measure the performance
+        for filename in (util.get_files_in_folder(test_folder)):
+            # Classify
+            label,log_posterior = classify_new_email(filename,
+                                                     probabilities_by_category,
+                                                     priors_by_category, hypothesis_crit)
+
+            # Measure performance (the filename indicates the true label)
+            base = os.path.basename(filename)
+            true_index = ('ham' in base)
+            guessed_index = (label == 'ham')
+            performance_measures[int(true_index), int(guessed_index)] += 1
+
+        # template="You correctly classified %d out of %d spam emails, and %d out of %d ham emails."
+        # Correct counts are on the diagonal
+        correct = np.diag(performance_measures)
+        # totals are obtained by summing across guessed labels
+        totals = np.sum(performance_measures, 1)
+        # print(template % (correct[0],totals[0],correct[1],totals[1]))
+        error1 = (totals[0] - correct[0]) / totals[0]
+        error2 = (totals[1] - correct[1]) / totals[1]
+        error1_set.append(error1)
+        error2_set.append(error2)
+
+    plt.figure()
+    plt.title("Trade off between Type1 and Type 2")
+    plt.scatter(error1_set, error2_set)
+    plt.xlabel('Type1 Error')
+    plt.ylabel('Type2 Error')
+    plt.grid()
+    plt.savefig("nbc.pdf")
--- Lab1/ldaqda/ldaqda.py
+++ Lab1/ldaqda/ldaqda.py
@@ -5,64 +5,210 @@ import util
 def discrimAnalysis(x, y):
     """
     Estimate the parameters in LDA/QDA and visualize the LDA/QDA models
-    
+
     Inputs
     ------
     x: a N-by-2 2D array contains the height/weight data of the N samples
-    
-    y: a N-by-1 1D array contains the labels of the N samples 
-    
+
+    y: a N-by-1 1D array contains the labels of the N samples
+
     Outputs
     -----
     A tuple of five elments: mu_male,mu_female,cov,cov_male,cov_female
     in which mu_male, mu_female are mean vectors (as 1D arrays)
              cov, cov_male, cov_female are covariance matrices (as 2D arrays)
-    Besides producing the five outputs, you need also to plot 1 figure for LDA 
-    and 1 figure for QDA in this function         
+    Besides producing the five outputs, you need also to plot 1 figure for LDA
+    and 1 figure for QDA in this function
     """
-    ### TODO: Write your code here
-    
-    
+
+    N = len(x)
+
+    male_height = []
+    male_weight = []
+    female_height = []
+    female_weight = []
+
+    for index in range(x.shape[0]):
+        if y[index] == 1:
+            male_height.append(x[index][0])
+            male_weight.append(x[index][1])
+        else:
+            female_height.append(x[index][0])
+            female_weight.append(x[index][1])
+
+
+    # compute mu_male, mu_female
+    mu_male_height = sum(male_height) / len(male_height)
+    mu_male_weight = sum(male_weight) / len(male_weight)
+    mu_female_height = sum(female_height) / len(female_height)
+    mu_female_weight = sum(female_weight) / len(female_weight)
+
+    mu_male = np.array([mu_male_height, mu_male_weight])
+    mu_female = np.array([mu_female_height, mu_female_weight])
+
+
+    # compute cov
+    mu_overall = np.sum(x, axis = 0) / len(x)
+    cov = np.zeros([2, 2])
+    cov[0][0] = (x[:,0] - mu_overall[0]).T @ (x[:,0] - mu_overall[0]) / N
+    cov[0][1] = (x[:,0] - mu_overall[0]).T @ (x[:,1] - mu_overall[1]) / N
+    cov[1][0] = (x[:,1] - mu_overall[1]).T @ (x[:,0] - mu_overall[0]) / N
+    cov[1][1] = (x[:,1] - mu_overall[1]).T @ (x[:,1] - mu_overall[1]) / N
+
+    # compute cov_male
+    cov_male = np.zeros([2, 2])
+    cov_male[0][0] = (male_height - mu_male_height).T @ (male_height - mu_male_height) / len(male_height)
+    cov_male[0][1] = (male_height - mu_male_height).T @ (male_weight - mu_male_weight) / len(male_height)
+    cov_male[1][0] = (male_weight - mu_male_weight).T @ (male_height - mu_male_height) / len(male_height)
+    cov_male[1][1] = (male_weight - mu_male_weight).T @ (male_weight - mu_male_weight) / len(male_height)
+
+    # compute cov_female
+    cov_female = np.zeros([2, 2])
+    cov_female[0][0] = (female_height - mu_female_height).T @ (female_height - mu_female_height) / len(female_height)
+    cov_female[0][1] = (female_height - mu_female_height).T @ (female_weight - mu_female_weight) / len(female_height)
+    cov_female[1][0] = (female_weight - mu_female_weight).T @ (female_height - mu_female_height) / len(female_height)
+    cov_female[1][1] = (female_weight - mu_female_weight).T @ (female_weight - mu_female_weight) / len(female_height)
+
+    plt.figure()
+    plt.grid()
+    plt.title("Linear Discriminant Analysis")
+    plt.scatter(male_height, male_weight, color = 'blue')
+    plt.scatter(female_height, female_weight, color = 'red')
+    plt.xlabel('Height')
+    plt.ylabel('Weight')
+    plt.xlim(50, 80)
+    plt.ylim(80, 280)
+    h = np.arange(50, 80)
+    w = np.arange(80, 280)
+    H, W = np.meshgrid(h, w)
+    male = np.zeros([200, 30])
+    female = np.zeros([200, 30])
+    for row in range(50,80):
+        rc = []
+        for col in range(80,280):
+            rc.append([row,col])
+        male[:,row - 50] = density_Gaussian(mu_male, cov, np.array(rc))
+        female[:,row -80] = density_Gaussian(mu_female, cov, np.array(rc))
+    beta1 = np.linalg.inv(cov) @ mu_male
+    gamma1 = -(mu_male.T @ np.linalg.inv(cov) @ mu_male) / 2
+    beta2 = np.linalg.inv(cov) @ mu_female
+    gamma2 = -(mu_female.T @ np.linalg.inv(cov) @ mu_female) / 2
+    y_lda = gamma1 + beta1[0] * H + beta1[1] * W - gamma2 - beta2[0] * H - beta2[1] * W
+    plt.contour(H, W, y_lda, 0)
+    plt.contour(H, W, male, linestyles='dashed')
+    plt.contour(H, W, female, linestyles='dashed')
+    plt.savefig("LDA.pdf")
+
+    plt.figure()
+    plt.grid()
+    plt.title("Quadratic Discriminant Analysis")
+    plt.scatter(male_height, male_weight, color = 'blue')
+    plt.scatter(female_height, female_weight, color = 'red')
+    plt.xlabel('Height')
+    plt.ylabel('Weight')
+    plt.xlim(50, 80)
+    plt.ylim(80, 280)
+    h = np.arange(50, 80)
+    w = np.arange(80, 280)
+    H, W = np.meshgrid(h, w)
+    male = np.zeros([200, 30])
+    female = np.zeros([200, 30])
+    for row in range(50,80):
+        rc = []
+        for col in range(80,280):
+            rc.append([row,col])
+        male[:,row - 50] = density_Gaussian(mu_male, cov_male, np.array(rc))
+        female[:,row -80] = density_Gaussian(mu_female, cov_female, np.array(rc))
+
+    y_qda = male - female
+    plt.contour(H, W, y_qda, 0)
+    plt.contour(H, W, male, linestyles='dashed')
+    plt.contour(H, W, female, linestyles='dashed')
+    plt.savefig("QDA.pdf")
+
+
     return (mu_male,mu_female,cov,cov_male,cov_female)
-    
+
 
 def misRate(mu_male,mu_female,cov,cov_male,cov_female,x,y):
     """
     Use LDA/QDA on the testing set and compute the misclassification rate
-    
+
     Inputs
     ------
     mu_male,mu_female,cov,cov_male,mu_female: parameters from discrimAnalysis
-    
-    x: a N-by-2 2D array contains the height/weight data of the N samples  
-    
-    y: a N-by-1 1D array contains the labels of the N samples 
-    
+
+    x: a N-by-2 2D array contains the height/weight data of the N samples
+
+    y: a N-by-1 1D array contains the labels of the N samples
+
     Outputs
     -----
-    A tuple of two elements: (mis rate in LDA, mis rate in QDA )
+    A tuple of two elements: (mis rate in LDA, mis rate in QDA)
     """
-    ### TODO: Write your code here
-   
-    
+
+    # Linear Discriminant Analysis
+    beta1 = np.linalg.inv(cov) @ mu_male
+    gamma1 = -(mu_male.T @ np.linalg.inv(cov) @ mu_male) / 2
+    beta2 = np.linalg.inv(cov) @ mu_female
+    gamma2 = -(mu_female.T @ np.linalg.inv(cov) @ mu_female) / 2
+
+    y_lda = gamma1 + beta1 @ x.T - gamma2 - beta2 @ x.T
+
+    for index in range(y_lda.shape[0]):
+        if y_lda[index] > 0:
+            y_lda[index] = 1
+        else:
+            y_lda[index] = 2
+
+    comparison = y_lda - y
+    miss_class = 0
+    for difference in comparison:
+        if difference != 0:
+            miss_class += 1
+
+    mis_lda = miss_class / len(y)
+
+
+    # Quadratic Discriminant Analysis
+    y_hat1 = np.zeros(len(y))
+    y_hat2 = np.zeros(len(y))
+
+    for i in range(x.shape[0]):
+        y_hat1[i] = - np.dot((x[i].T - mu_male) @ np.linalg.inv(cov_male),(x[i] - mu_male)) - np.log(np.linalg.det(cov_male))
+        y_hat2[i] = - np.dot((x[i].T - mu_female) @ np.linalg.inv(cov_female),(x[i] - mu_female)) - np.log(np.linalg.det(cov_female))
+
+    y_qda = y_hat1 - y_hat2
+
+    for index in range(y_qda.shape[0]):
+        if y_qda[index] > 0:
+            y_qda[index] = 1
+        else:
+            y_qda[index] = 2
+
+    comparison = y_qda - y
+    miss_class = 0
+    for difference in comparison:
+        if difference != 0:
+            miss_class += 1
+
+    mis_qda = miss_class / len(y)
+
+
     return (mis_lda, mis_qda)
 
 
 if __name__ == '__main__':
-    
+
     # load training data and testing data
     x_train, y_train = util.get_data_in_file('trainHeightWeight.txt')
     x_test, y_test = util.get_data_in_file('testHeightWeight.txt')
-    
+
     # parameter estimation and visualization in LDA/QDA
     mu_male,mu_female,cov,cov_male,cov_female = discrimAnalysis(x_train,y_train)
-    
+
     # misclassification rate computation
     mis_LDA,mis_QDA = misRate(mu_male,mu_female,cov,cov_male,cov_female,x_test,y_test)
-    
 
-    
-    
-    
-
-    
+    print('miss rate of LDA = {}'.format(mis_LDA))
+    print('miss rate of QDA = {}'.format(mis_QDA))
